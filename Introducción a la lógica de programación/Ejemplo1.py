add100629fc16b2efa63f8ad87dd929c6bd2c0b5
LuisCruz30 <123257373+LuisCruz30@users.noreply.github.com>
2023-01-21 11:20:23 -0600

    Ejemplo1

---
+++ Introducción a la lógica de programación/Ejemplo1.py
@@ -0,0 +1,14 @@
+# EJEMPLO 1. SALUDO
+# COMENTARIOS: Sirven para detallar y especificar cada parte del código
+# A los comentarios los omite o no los toma en cuenta Python
+
+'''
+COMENTARIOS
+'''
+
+#Imprimir en pantalla o mostrar resultados
+print(5+5) # Suma
+print(10-2) # Resta
+print ("Hola mundo")
+print ('Eduardo');
+print(13)
