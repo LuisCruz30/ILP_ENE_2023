9beb4b3088c8a4c6eb9e2ca245c1257f722f9023
LuisCruz30 <123257373+LuisCruz30@users.noreply.github.com>
2023-02-11 09:16:00 -0600

    Pedir un número y decir si es par o impar.

---
+++ Ejercicio 6.py
@@ -0,0 +1,12 @@
+#Ej. 6. Pedir un número y decir si es par o impar.
+
+#ENTRADA DE DATOS
+n = int(input("Escribir un número: "))
+
+if n % 2 == 0:
+
+    print(f"El numero [n] es par")
+
+else:
+
+    print(f"El numero[n] es impar")
